03ead3a3067ffedfceb8ed7fad4decdcadbdce7d
Garrett Malmquist <garrett.malmquist@gmail.com>
2015-02-04 17:07:07 -0500

    docs are good.

--- src/main/java/edu/gatech/cs2340/trydent/Behavior.java
+++ src/main/java/edu/gatech/cs2340/trydent/Behavior.java
@@ -1,5 +1,9 @@
 package edu.gatech.cs2340.trydent;
 
+/**
+ * Behavior class for adding scripts (behaviors) to GameObjects.
+ * @author Garrett Malmquist
+ */
 public abstract class Behavior extends ContinuousEvent {
 
     private GameObject object;
