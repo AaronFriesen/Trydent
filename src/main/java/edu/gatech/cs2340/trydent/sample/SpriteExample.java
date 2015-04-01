602fc867a32ab0e608bc97d31b4303b4f4abc5a2
winterfroststrom <winterfroststorm@gmail.com>
2015-04-01 12:18:08 -0400

    fixed jar resources issue

--- src/main/java/edu/gatech/cs2340/trydent/sample/SpriteExample.java
+++ src/main/java/edu/gatech/cs2340/trydent/sample/SpriteExample.java
@@ -23,7 +23,7 @@ public class SpriteExample implements Runnable {
 
     @Override
     public void run() {
-        String prefix = "edu/gatech/cs2340/trydent/sample/res/";
+        String prefix = "edu/gatech/cs2340/trydent/sample/sprites/";
         GameObject pikachu = new Sprite(prefix + "pikapika.gif");
         pikachu.setLocalPosition(new Position(50, 50));
 
--- src/main/java/edu/gatech/cs2340/trydent/sample/tictactoe/TicTacToeExample.java
+++ src/main/java/edu/gatech/cs2340/trydent/sample/tictactoe/TicTacToeExample.java
@@ -2,18 +2,19 @@ package edu.gatech.cs2340.trydent.sample.tictactoe;
 
 import edu.gatech.cs2340.trydent.TrydentEngine;
 
-public class TicTacToe implements Runnable {
+public class TicTacToeExample implements Runnable {
+    private static final String TITLE_SCREEN = "edu/gatech/cs2340/trydent/sample/tictactoe/TicTacToeTitle.fxml";
 
     public static void main(String[] args) {
         TrydentEngine.start();
         TrydentEngine.setWindowTitle("TrydentEngine - Tic-Tac-Toe");
         TrydentEngine.setWindowSize(720, 480);
-        TrydentEngine.runOnce(new TicTacToe());
+        TrydentEngine.runOnce(new TicTacToeExample());
     }
 
     @Override
     public void run() {
-        TrydentEngine.setForeground(getClass().getResource("TicTacToeTitle.fxml"));
+        TrydentEngine.setForeground(getClass().getClassLoader().getResourceAsStream(TITLE_SCREEN));
     }
 
 }
